1245af1dd3257fb4036246ce2991d6504a571b91
estebanwasinger <estebanwasinger@gmail.com>
2017-09-22 13:29:58 -0300

    MULE-13521: JMS does not communicate properly it's ExternalLibs

--- src/main/java/org/mule/extensions/jms/internal/JmsConnector.java
+++ src/main/java/org/mule/extensions/jms/internal/JmsConnector.java
@@ -1,11 +1,11 @@
-package org.mule.extensions.jms.internal;
-
 /*
  * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
+package org.mule.extensions.jms.internal;
+
 import org.mule.extensions.jms.api.connection.caching.CachingStrategy;
 import org.mule.extensions.jms.api.connection.caching.DefaultCachingStrategy;
 import org.mule.extensions.jms.api.connection.caching.NoCachingConfiguration;
