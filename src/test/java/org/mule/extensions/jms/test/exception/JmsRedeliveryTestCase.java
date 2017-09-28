d50060fd97b73d16165f3ac063c5707ad377d9f1
Gustavo Adolfo Rodriguez <gustavo.libreros@mulesoft.com>
2017-09-27 21:32:54 -0500

    MULE-10962: Migrate Basic Tests from JMS transport to JMS Extension
     - Migration of basic tests based on Mule 3.x JMS transport tests

---
+++ src/test/java/org/mule/extensions/jms/test/exception/JmsRedeliveryTestCase.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extensions.jms.test.exception;
+
+import static java.util.Arrays.asList;
+import static org.junit.runners.Parameterized.Parameter;
+import static org.junit.runners.Parameterized.Parameters;
+import static org.mule.extensions.jms.test.AllureConstants.JmsFeature.JMS_EXTENSION;
+import static org.mule.extensions.jms.test.AllureConstants.JmsFeature.JmsStory.REDELIVERY;
+
+import org.mule.extensions.jms.test.JmsAbstractTestCase;
+import org.mule.functional.api.exception.FunctionalTestException;
+import org.mule.runtime.api.exception.MuleException;
+import org.mule.runtime.core.api.event.CoreEvent;
+import org.mule.runtime.core.api.processor.Processor;
+import org.mule.tck.junit4.rule.SystemProperty;
+import org.mule.tck.junit4.rule.SystemPropertyLambda;
+import org.mule.tck.probe.JUnitLambdaProbe;
+import org.mule.tck.probe.PollingProber;
+import org.mule.test.runner.RunnerDelegateTo;
+
+import java.util.Collection;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+@Feature(JMS_EXTENSION)
+@Story(REDELIVERY)
+@RunnerDelegateTo(Parameterized.class)
+public class JmsRedeliveryTestCase extends JmsAbstractTestCase {
+
+  private static final long REDELIVERY_TIMEOUT = 90000;
+  private static final int POLL_DELAY_MILLIS = 100;
+  public static final int REDELIVERY_IGNORE = -1;
+  public static final int EXPECTED_REDELIVERS_WHEN_MAX_IGNORED = 5;
+
+  private static int numberOfDeliveries;
+
+  @Parameter
+  public int maxRedeliveryAttempts;
+
+  @Parameters(name = "maxRedelivery: {0}")
+  public static Collection<Object[]> data() {
+    return asList(new Object[][] {{0}, {3}, {REDELIVERY_IGNORE}});
+  }
+
+  @Rule
+  public SystemProperty destination = new SystemProperty("destination", newDestination("destination"));
+
+  @Rule
+  public SystemProperty maxRedelivery = new SystemPropertyLambda(MAX_REDELIVERY, this::getMaxRedeliveryAttempts);
+
+  @Override
+  protected String[] getConfigFiles() {
+    return new String[] {"config/activemq/activemq-default.xml", "exception/jms-redelivery-flow.xml"};
+  }
+
+  @Before
+  public void setup() {
+    numberOfDeliveries = 0;
+  }
+
+  @Test
+  @Description("verifies that message redelivery attempts correspond to the configured value")
+  public void testMaxRedelivery() throws Exception {
+    publish(TEST_MESSAGE);
+    new PollingProber(REDELIVERY_TIMEOUT, POLL_DELAY_MILLIS)
+        .check(new JUnitLambdaProbe(this::hasReachedNumberOfExpectedRedelivers));
+  }
+
+  private boolean hasReachedNumberOfExpectedRedelivers() {
+    if (maxRedeliveryAttempts == REDELIVERY_IGNORE) {
+      return numberOfDeliveries > EXPECTED_REDELIVERS_WHEN_MAX_IGNORED;
+    } else {
+      return numberOfDeliveries == maxRedeliveryAttempts + 1;
+    }
+  }
+
+  private String getMaxRedeliveryAttempts() {
+    return String.valueOf(maxRedeliveryAttempts);
+  }
+
+  public static class TestProcessor implements Processor {
+
+    @Override
+    public CoreEvent process(CoreEvent event) throws MuleException {
+      numberOfDeliveries++;
+      throw new FunctionalTestException();
+    }
+  }
+
+}
--- src/test/java/org/mule/extensions/jms/test/queue/integration/JmsDynamicDestinationTestCase.java
+++ src/test/java/org/mule/extensions/jms/test/queue/integration/JmsDynamicDestinationTestCase.java
@@ -4,32 +4,24 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.extensions.jms.integration;
+package org.mule.extensions.jms.test.queue.integration;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mule.extensions.jms.test.AllureConstants.JmsFeature.JMS_EXTENSION;
-import static org.mule.extensions.jms.test.AllureConstants.JmsFeature.JmsStory.CORRELATION_ID;
+import static org.mule.functional.junit4.matchers.MessageMatchers.hasPayload;
 
-import org.mule.extensions.jms.api.message.JmsAttributes;
-import org.mule.extensions.jms.api.message.JmsHeaders;
 import org.mule.extensions.jms.test.JmsAbstractTestCase;
 import org.mule.runtime.api.message.Message;
-import org.mule.runtime.api.metadata.TypedValue;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Step;
-import io.qameta.allure.Story;
 import org.junit.Test;
 
 @Feature(JMS_EXTENSION)
-@Story(CORRELATION_ID)
-public class JmsCorrelationIdPropagationTestCase extends JmsAbstractTestCase {
+public class JmsDynamicDestinationTestCase extends JmsAbstractTestCase {
 
-  private static final String FIRST_MESSAGE = "My First Message";
   private static final String BRIDGE_FLOW = "bridge";
   private static final String SEND_PAYLOAD_FLOW = "send-payload";
   private static final String BRIDGE_RECEIVER_FLOW = "bridge-receiver";
@@ -40,41 +32,38 @@ public class JmsCorrelationIdPropagationTestCase extends JmsAbstractTestCase {
   private static final String FINAL_DESTINATION = "finalQueue";
   private static final String FINAL_DESTINATION_VAR = "finalDestination";
 
-  private static final String CUSTOM_CORRELATION_ID = "custom-correlation-id";
-  private static final String CUSTOM_CORRELATION_ID_VAR = "customCorrelationId";
-
   @Override
   protected String[] getConfigFiles() {
-    return new String[] {"config/activemq/activemq-default.xml", "integration/jms-correlation-id-propagation.xml"};
+    return new String[] {"config/activemq/activemq-default.xml", "integration/jms-dynamic-destination.xml"};
   }
 
   @Test
-  @Description("Tests the correct propagation of the correlation id property within the JMS connector.")
-  public void testMuleCorrelationIdPropagation() throws Exception {
+  @Description("Verifies that a message can be dynamically routed to a destination based on its content or properties. This was" +
+      " previously achieved through the usage of something called dynamic endpoints.")
+  public void testDynamicDestination() throws Exception {
     sendMessage();
     executeBridge();
-    Message message = receiveMessageFromBridgeTarget();
-    assertExpectedMessage(message);
+
+    assertExpectedMessage(receiveMessageFromBridgeTarget());
   }
 
   @Step("Send message")
   protected void sendMessage() throws Exception {
     flowRunner(SEND_PAYLOAD_FLOW)
         .withVariable(INITIAL_DESTINATION_VAR, INITIAL_DESTINATION)
-        .withVariable(CUSTOM_CORRELATION_ID_VAR, CUSTOM_CORRELATION_ID)
-        .withPayload(FIRST_MESSAGE)
+        .withVariable(FINAL_DESTINATION_VAR, FINAL_DESTINATION)
+        .withPayload(TEST_PAYLOAD)
         .run();
   }
 
-  @Step("Execute bridge")
+  @Step("Execute bridge flow")
   protected void executeBridge() throws Exception {
     flowRunner(BRIDGE_FLOW)
         .withVariable(INITIAL_DESTINATION_VAR, INITIAL_DESTINATION)
-        .withVariable(FINAL_DESTINATION_VAR, FINAL_DESTINATION)
         .run();
   }
 
-  @Step("Receive message from bridge target")
+  @Step("Receive message")
   protected Message receiveMessageFromBridgeTarget() throws Exception {
     return flowRunner(BRIDGE_RECEIVER_FLOW)
         .withVariable(FINAL_DESTINATION_VAR, FINAL_DESTINATION)
@@ -82,14 +71,8 @@ public class JmsCorrelationIdPropagationTestCase extends JmsAbstractTestCase {
         .getMessage();
   }
 
-  @Step("Verify correlation id properly was propagated")
+  @Step("Assert message")
   protected void assertExpectedMessage(Message message) {
-    TypedValue<Object> attributes = message.getAttributes();
-    assertThat(attributes, not(nullValue()));
-
-    JmsHeaders headers = ((JmsAttributes) attributes.getValue()).getHeaders();
-    assertThat(headers, not(nullValue()));
-    assertThat(headers.getJMSCorrelationID(), is(CUSTOM_CORRELATION_ID));
+    assertThat(message, hasPayload(equalTo(TEST_PAYLOAD)));
   }
-
 }
