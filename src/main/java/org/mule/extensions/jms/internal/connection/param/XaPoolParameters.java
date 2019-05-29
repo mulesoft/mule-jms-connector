04c692e1aef3e69bf6e31c5e03201abe3169b437
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2019-05-29 17:58:04 -0400

    MULE-16926: Make XA pool configurable in JMS connector

--- src/main/java/org/mule/extensions/jms/internal/connection/param/XaPoolParameters.java
+++ src/main/java/org/mule/extensions/jms/internal/connection/param/XaPoolParameters.java
@@ -6,21 +6,45 @@
  */
 package org.mule.extensions.jms.internal.connection.param;
 
+import static org.mule.runtime.extension.api.annotation.param.display.Placement.ADVANCED_TAB;
 import org.mule.runtime.extension.api.annotation.param.Optional;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
+import org.mule.runtime.extension.api.annotation.param.display.Placement;
+import org.mule.runtime.extension.api.annotation.param.display.Summary;
 
+/**
+ * Allows parametrization of the Bitronix connection pool that is created when XA is enabled.
+ *
+ * @since 1.5.0
+ */
 public class XaPoolParameters implements org.mule.jms.commons.internal.connection.param.XaPoolParameters {
 
+  /**
+   * The minimum size of the XA connection pool. This value is only considered when XA connections are used.
+   */
   @Parameter
   @Optional(defaultValue = "4")
+  @Summary("The minimum size of the XA connection pool.")
+  @Placement(tab = ADVANCED_TAB)
   private int minPoolSize = 4;
 
+  /**
+   * The maximum size of the XA connection pool. This value is only considered when XA connections are used.
+   */
   @Parameter
   @Optional(defaultValue = "32")
+  @Summary("The maximum size of the XA connection pool.")
+  @Placement(tab = ADVANCED_TAB)
   private int maxPoolSize = 32;
 
+  /**
+   * How many seconds can an XA transaction remain idle before its eligible for eviction.
+   * This value is only considered when XA connections are used.
+   */
   @Parameter
   @Optional(defaultValue = "60")
+  @Summary("How many seconds can an XA transaction remain idle before its eligible for eviction.")
+  @Placement(tab = ADVANCED_TAB)
   private int maxIdleSeconds = 60;
 
   @Override
