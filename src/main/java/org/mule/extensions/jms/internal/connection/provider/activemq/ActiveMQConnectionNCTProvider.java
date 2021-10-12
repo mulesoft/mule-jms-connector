dd8da45759ca6508221af339f57454b4c7bfc166
Alejandro Curci <76498083+acurci-at-mulesoft@users.noreply.github.com>
2021-10-12 13:09:32 -0300

    SE-20385: Creation of a new connection provider for ActiveMQ, without connectivity check prior to deployment. (#363)

---
+++ src/main/java/org/mule/extensions/jms/internal/connection/provider/activemq/ActiveMQConnectionNCTProvider.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extensions.jms.internal.connection.provider.activemq;
+
+import org.mule.runtime.extension.api.annotation.Alias;
+import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
+import org.mule.runtime.extension.api.connectivity.NoConnectivityTest;
+
+
+@DisplayName("ActiveMQ Connection - No Connectivity Test")
+@Alias("active-mq-nct")
+public class ActiveMQConnectionNCTProvider extends ActiveMQConnectionProvider implements NoConnectivityTest {
+
+}
