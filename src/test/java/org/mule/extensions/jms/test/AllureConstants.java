d2cc3f8f05919827eb45ee39077d3d6d1b40348d
juan.flores <juan.flores@mulesoft.com>
2023-08-15 14:08:00 -0300

    Revert "Refactor and tests"
    This reverts commit 02764d106f5fec46b28e02a42d36613ddb5ad0fb.

--- src/test/java/org/mule/extensions/jms/test/AllureConstants.java
+++ src/test/java/org/mule/extensions/jms/test/AllureConstants.java
@@ -29,8 +29,6 @@ public class AllureConstants {
       String MESSAGE_TYPES = "Durable Subscriber";
 
       String MESSAGE_FILTERING = "Message Filtering";
-
-      String TRACING = "Tracing";
     }
 
   }
