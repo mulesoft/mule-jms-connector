3bf54376878d46cb3bb51fc7bb61888668ee7f65
juan.flores <juan.flores@mulesoft.com>
2025-01-16 14:41:23 -0300

    fix failover brokerUrl format

--- src/main/java/org/mule/extensions/jms/internal/util/ActiveMQConnectionFactoryUtil.java
+++ src/main/java/org/mule/extensions/jms/internal/util/ActiveMQConnectionFactoryUtil.java
@@ -6,8 +6,10 @@
  */
 package org.mule.extensions.jms.internal.util;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class ActiveMQConnectionFactoryUtil {
 
@@ -15,6 +17,8 @@ public class ActiveMQConnectionFactoryUtil {
   private static final int MINOR_VERSION_THRESHOLD = 15;
   private static final int PATCH_VERSION_THRESHOLD = 6;
   private static final String REX_PATTER = "(\\d+)\\.(\\d+)\\.(\\d+)";
+  private static final String HOSTS_MATCHER = "\\((.*?)\\)" ;
+  private static final String VERIFY_HOSTNAME = "socket.verifyHostName";
 
   /**
    * Utility that helps us determine if the activeMQ client version is lower than the limit version to configure the VerifyHostName parameter
@@ -42,5 +46,26 @@ public class ActiveMQConnectionFactoryUtil {
       return false;
     }
     return patchVersion >= PATCH_VERSION_THRESHOLD;
+  }
+
+  public static String brokerUrlFormat(String brokerURL, boolean verifyHostName) {
+    int parametersIndex = brokerURL.indexOf("?");
+    String queryParameters = "";
+    if (parametersIndex != -1) {
+      queryParameters = brokerURL.substring(parametersIndex);
+    }
+    Pattern pattern = Pattern.compile(HOSTS_MATCHER);
+    Matcher matcher = pattern.matcher(brokerURL);
+    if (matcher.find()) {
+      String failoverUrl = matcher.group(1);
+      String addedVerifyHostName = Arrays.stream(failoverUrl.split(","))
+          .map(element -> element + "?" + VERIFY_HOSTNAME + "=" + verifyHostName)
+          .collect(Collectors.joining(","));
+      brokerURL = "failover:(" + addedVerifyHostName + ")";
+    }
+    if (!queryParameters.isEmpty()) {
+      brokerURL += queryParameters;
+    }
+    return brokerURL;
   }
 }
